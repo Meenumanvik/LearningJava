6b91bb251afac5e007b3264a08afa3ec361e1b81
Meenumanvik <meenumanvik2022@gmail.com>
2025-02-18 19:06:03 +0530

    17nd Feb Prac

---
+++ src/Ex10Loop/LoopExample.java
@@ -0,0 +1,9 @@
+package Ex10Loop;
+
+public class LoopExample {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++)
+            System.out.println(i);
+        // 0,1,2......10 - times it got executed - 10 times ( 0 to 10)
+    }
+}
---
+++ src/Ex10Loop/LoopExample1.java
@@ -0,0 +1,17 @@
+package Ex10Loop;
+
+public class LoopExample1 {
+    public static void main(String[] args) {
+ //       for (int rohit=0;rohit < 10;rohit++){
+//            System.out.println(rohit);
+//        }
+
+            for (int rohit = 1; rohit <= 10; ++rohit) {
+                System.out.println(rohit); // 11 times, print 0 to 10
+            }
+
+            for (int _1 = 0; _1 <= 10; _1++) { // 11 times
+                System.out.println(_1); // 0, to 10
+            }
+    }
+}
