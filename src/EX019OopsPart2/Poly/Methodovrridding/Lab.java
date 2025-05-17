db1027f67fca81f6ac26349c010100041eea786c
Meenumanvik <meenumanvik2022@gmail.com>
2025-05-17 22:57:31 +0530

    17th May

--- src/EX019OopsPart2/Poly/Methodovrridding/Lab.java
+++ src/EX019OopsPart2/Poly/Methodovrridding/Lab.java
@@ -1,11 +1,17 @@
 package EX019OopsPart2.Poly.Methodovrridding;
 
-import EX019OopsPart2.inheritence.hierarchical.Meenu;
 
 public class Lab {
     public static void main(String args[])
     {
         Meenu m=new Meenu();
+        m.home();
+        m.m1();
+
+
+        Father f1=new Father();
+        f1.home();
+        f1.f1();
 
     }
 }
